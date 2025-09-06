2f0e42b94dafddbe1dea9da6b9f5fd2c4ce1d6ac
george <gseruma408@gmail.com>
2025-09-06 21:25:58 +0300

    I have finished task04 from lecture 02

---
+++ lecture02/task04.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number(a):"))
+b = int(input("Enter second number(b):"))
+c = int(input("Enter third number(c):"))
+sum = a+b+c
+print("sum: ", sum)
+#toget average we add the items and divide with total number of items
+average = (a+b+c)/3
+print("average: ", average)
+product = (a*b*c)
+print("product: ", product)
