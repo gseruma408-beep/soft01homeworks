ca56050238be3d9cfdd66df2ba9a54b3ed02278f
george <gseruma408@gmail.com>
2025-09-06 21:26:36 +0300

    I have finished task06 from lecture 02

---
+++ lecture02/task06.py
@@ -0,0 +1,14 @@
+import random
+#A 3-digit code where each number is between 0 and 9.
+digit1 = random.randint(0,9)
+digit2 = random.randint(0,9)
+digit3 = random.randint(0,9)
+print(f"combination of 3: {digit1}{digit2}{digit3}")
+
+
+#A 4-digit code where each number is between 1 and 6.
+digit4 = random.randint(1,6)
+digit5 = random.randint(1,6)
+digit6 = random.randint(1,6)
+digit7 = random.randint(1,6)
+print(f"combination of 4: {digit4}{digit5}{digit6}{digit7}")
