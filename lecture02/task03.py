84b33497e9de8cd822bb49c607116d79d024bf3d
george <gseruma408@gmail.com>
2025-09-06 21:25:39 +0300

    I have finished task03 from lecture 02

---
+++ lecture02/task03.py
@@ -0,0 +1,13 @@
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+perimeter = 2*(length + width)
+print("perimeter:", perimeter)
+
+area = length*width
+print("Area;",area)
+
+
+
+
+
+9
